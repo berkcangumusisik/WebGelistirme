dad8df97abf9f908ac3bd26ff380997b706f7c0e
Berkcan Gümüşışık <gumusisikberkcan@gmail.com>
2022-02-09 17:17:12 +0300

    Javascripte giriş yapıldı.

---
+++ 8.Bölüm Javascript/1.JS'e Giris/script.js
@@ -0,0 +1,14 @@
+alert("Hello")
+console.log("Merhaba")
+console.log(1234)
+console.log(true)
+console.log(1,2,3,4)
+console.error("Hata oluştu")
+console.warn("Bir uyarı oluştu")
+
+//Tek satırlık yorum satırı
+
+/**
+ * 
+ * Çoklu yorum satırı
+ */
